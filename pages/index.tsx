dbf8294fbfaeb3aa1d7ff5e0dd240f59fca8132e
oscelest <oscelest@gmail.com>
2023-03-08 17:20:22 +0100

    No test page yet.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,10 @@
 import React from "react";
-import {createSubscription, useSubscription} from "../src";
-import Style from "./index.module.css";
-
-export const subscription = createSubscription<number>(0);
 
 function IndexPage() {
-  const [value, setValue] = useSubscription(subscription);
-  const [value2, setValue2] = useSubscription(subscription);
   
   return (
-    <div className={Style.Component}>
-      <div>
-        <span>Counter:</span>
-        <span>{value}</span>
-        <span>{value2}</span>
-      </div>
-      <button onClick={onComponentClick}>Click me for 1!</button>
-      <button onClick={onComponentClick2}>Click me for 2!</button>
-    </div>
+    <div>Hello World!</div>
   );
-  
-  function onComponentClick() {
-    setValue(value + 1);
-  }
-  
-  function onComponentClick2() {
-    setValue2(value => value + 2);
-  }
 }
 
 export default IndexPage;
