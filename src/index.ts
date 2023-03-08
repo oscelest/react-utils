0ed8db5b1c1025373301f8d5fede24e323584d89
oscelest <oscelest@gmail.com>
2023-03-08 17:14:36 +0100

    Updates.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+import {DetailedHTMLProps, HTMLAttributes} from "react";
+
 export * from "./functions";
+
+export type HTMLComponentProps<E = HTMLDivElement> = DetailedHTMLProps<HTMLAttributes<E>, E>
